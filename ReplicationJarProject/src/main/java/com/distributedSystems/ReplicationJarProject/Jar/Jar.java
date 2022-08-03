f3ed3f4299094c220c9e3fd65f08e086dc820fe8
RafaDVB <rafael.v.barrientos@gmail.com>
2022-08-02 20:03:08 -0400

    Arreglado error al escribir JSON

--- ReplicationJarProject/src/main/java/com/distributedSystems/ReplicationJarProject/Jar/Jar.java
+++ ReplicationJarProject/src/main/java/com/distributedSystems/ReplicationJarProject/Jar/Jar.java
@@ -1,5 +1,6 @@
 package com.distributedSystems.ReplicationJarProject.Jar;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.rmi.AlreadyBoundException;
 import java.rmi.Remote;
@@ -9,11 +10,14 @@ import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.List;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class Jar {
     private static final int port = 8099;
     private static FileWriter file;
+    private static FileReader reader;
     public static void main(String[] args) throws RemoteException, AlreadyBoundException{
         Remote remote = UnicastRemoteObject.exportObject(new JarInterface() {
 
@@ -30,12 +34,28 @@ public class Jar {
 
             @Override
             public void logTransaction(Register register) throws RemoteException {
+                JSONParser parser = new JSONParser();
+                JSONArray movementList = new JSONArray();
+                try {
+                    reader = new FileReader("movements.json");
+                    Object object = parser.parse(reader);
+                    movementList = (JSONArray) object;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        reader.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
                 JSONObject object = new JSONObject();
                 object.put("name", register.getName());
                 object.put("date", register.getDate().toString());
+                movementList.add(object);
                 try {
                     file = new FileWriter("movements.json");
-                    file.write(object.toJSONString());
+                    file.write(movementList.toJSONString());
                     System.out.println("Logged " + object);
                 } catch (Exception e) {
                     e.printStackTrace();
